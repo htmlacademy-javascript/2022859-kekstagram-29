62219953e8935dc0e97939a26ae12b6447091bb7
Yaroslav <yibelyaev@icloud.com>
2023-06-29 06:45:49 +0300

    fix fragment

--- js/post.js
+++ js/post.js
@@ -6,7 +6,7 @@ const postList = document.querySelector('.pictures');
 //Записываем функию с объектами
 const similarPosts = createPosts();
 //записываем фрагмент('Черная коробка')
-const postListFragment = document.createDocumentFragment;
+const postListFragment = document.createDocumentFragment();
 //Перебираем массив и добавляем элементы и данные для них
 similarPosts.forEach(({ url, description, likes, comments }) => {
   const postItem = postTemplate.cloneNode(true);
@@ -14,7 +14,9 @@ similarPosts.forEach(({ url, description, likes, comments }) => {
   postItem.querySelector('.picture__img').alt = description;//Описание фото
   postItem.querySelector('.picture__likes').textContent = likes;//Число лайков
   postItem.querySelector('.picture__comments').textContent = comments.length;//Число комментариев
-  postList.append(postItem);//Добавляем все в элемент
+  postListFragment.appendChild(postItem);//Добавляем все в элемент
 });
 //Выгружаем в фрагмент
 postList.appendChild(postListFragment);
+
+
