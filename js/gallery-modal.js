aa5d49a0527af32eb6f3f97d3ee19aa24c2d1d16
Yaroslav <yibelyaev@icloud.com>
2023-07-06 20:02:25 +0300

    added function comments more

--- js/gallery-modal.js
+++ js/gallery-modal.js
@@ -1,16 +1,15 @@
 import { openPictureModal } from './modal.js';
-// import { createComment } from './create-element.js';
 import { createElemet } from './utils.js';
 
 const bigPictureImg = document.querySelector('.big-picture__img img');
 const likeCount = document.querySelector('.likes-count');
-const commentCount = document.querySelector('.comments-count');
+const showCount = document.querySelector('.social__comment-count');
+const commentCount = showCount.querySelector('.comments-count');
 const commentsDescription = document.querySelector('.social__caption');
-const loadButton = document.querySelector('.comments-loader');
+const moreButton = document.querySelector('.comments-loader');
 const commentsList = document.querySelector('.social__comments');
-const blockCount = document.querySelector('.social__comment-count');
 const COMMENTS_TO_SHOW = 5;
-// let currentComments = [];
+let currentComments = [];
 
 
 const createComment = (avatar, message, name) => {
@@ -39,23 +38,29 @@ const renderComments = (commentsData) => {
   return fragment;
 };
 
-const initFirstComments = (comments) => {
-  const showComments = comments.slice(0, COMMENTS_TO_SHOW);
-  const renderFirstComments = renderComments(showComments);
 
+const initLoadMoreComments = (comments) => {
+  const toShowComments = comments.slice(0, COMMENTS_TO_SHOW,);
+  const renderFirstComments = renderComments(toShowComments);
+
+  if (toShowComments.length === comments.length) {
+    moreButton.classList.add('hidden');
+  }
 
   commentsList.append(renderFirstComments);
-
-  loadButton.addEventListener('click', (items) => {
-    commentsList.append(...items).map(renderComments);
-  });
-  blockCount.firstChild.textContent = `${showComments.length} из `;
+  showCount.firstChild.textContent = `${toShowComments.length} из `;
 };
 
 const loadMore = () => {
+  const moreComments = currentComments.slice(commentsList.children.length, commentsList.children.length + COMMENTS_TO_SHOW);
+  const renderMoreComments = renderComments(moreComments);
+  commentsList.append(renderMoreComments);
 
-}
-
+  if (currentComments.length === commentsList.children.length) {
+    moreButton.classList.add('hidden');
+  }
+  showCount.firstChild.textContent = `${commentsList.children.length} из `;
+};
 
 const createPictureModal = (postData) => {
   const {url, likes, comments, description} = postData;
@@ -63,11 +68,11 @@ const createPictureModal = (postData) => {
   likeCount.textContent = likes;
   commentCount.textContent = comments.length;
   commentsDescription.textContent = description;
-
   commentsList.innerHTML = '';
-  initFirstComments(comments);
-
+  currentComments = comments;
+  moreButton.addEventListener('click', loadMore);
+  initLoadMoreComments(comments);
   openPictureModal(postData);
 };
 
-export { createPictureModal, renderComments };
+export { createPictureModal };
